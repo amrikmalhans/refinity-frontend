d1bd4e1ec8ac72dad2bc5a3a5ab690df7cc497e1
amrik malhans <amrikdeveloper@gmail.com>
2020-07-23 09:25:27 +0530

    making carousel

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,49 +1,43 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 
- const Mao = () => {
+ const Car = () => {
 
     return (
         <>
             <Carousel>
             <Carousel.Item>
               <img
+                width="100%"
+                height="auto"
                 className="d-block w-100"
-                src=""
+                src="/dummy.jpg"
                 alt="First slide"
               />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
+  
             </Carousel.Item>
             <Carousel.Item>
               <img
+                width="100%"
+                height="auto"
                 className="d-block w-100"
-                src="4"
+                src="/dummy.jpg"
                 alt="Third slide"
               />
 
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
+                width="100%"
+                height="auto"
                 className="d-block w-100"
-                src=""
+                src="/dummy.jpg"
                 alt="Third slide"
               />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         </>
     )
 }
 
-export default Mao
+export default Car
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
-import Mao from '../components/Carousel'
+import Car from '../components/Carousel'
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <Header />
-        <Mao />
+        <Car />
       </main>
     </div>
   )
