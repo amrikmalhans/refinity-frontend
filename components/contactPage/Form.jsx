8499766f8d5bff19ed1fa3c7e8bd684717608895
amrik malhans <amrikdeveloper@gmail.com>
2020-07-31 16:43:46 +0530

    making form

--- components/contactPage/Form.jsx
+++ components/contactPage/Form.jsx
@@ -4,12 +4,9 @@ import styles from '../../styles/contactPage/form.module.css'
 export const ContactForm = () => {
     return (
         <form className={styles.Form}>
-            <label for="name">Full name:</label><br />
-            <input placeholder="full name" type="text" id="name" name="name" /><br />
-            <label for="email">Email:</label><br />
-            <input placeholder="email" type="email" id="email" name="email" /><br />
-            <label for="message">Your Question:</label><br />
-            <textarea placeholder="Type your questions here...." required></textarea> <br />
+            <input placeholder="full name" type="text" id="name" name="name" /> 
+            <input placeholder="email" type="email" id="email" name="email" />  
+            <textarea placeholder="Type your questions here...." required></textarea>   
             <input type="submit" value="Submit" />
         </form>
     )
