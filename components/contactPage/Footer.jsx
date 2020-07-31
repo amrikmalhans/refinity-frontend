08b145aecc0a451261a3c10445ea4eb8bfb1b938
amrik malhans <amrikdeveloper@gmail.com>
2020-07-31 23:19:02 +0530

    made footer

--- components/contactPage/Footer.jsx
+++ components/contactPage/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import styles from '../../styles/contactPage/footer.module.css'
+import { FaInstagram } from 'react-icons/fa'
+import { FiTwitter } from 'react-icons/fi'
+import { FcReddit } from 'react-icons/fc'
 
 export const Footer = () => {
     return (
-        <footer>
-            hello
+        <footer className={styles.Footer}>
+            <p>Welcome to Refinity! Here are all the platforms you can support us through or reach out to us on -</p>
+            <div className={styles.items}>
+                <div className={styles.SocialIcons}>
+                    <FaInstagram />
+                    <FiTwitter />
+                    <FcReddit />
+                </div>
+                <div className={styles.Emails}>
+                    <div>
+                        <span>Academics - </span>
+                        <a href="">academics@refinityedu.org</a>
+                    </div>
+                    <div>
+                        <span>Public Relations - </span>
+                        <a href="">relations@refinityedu.org</a>
+                    </div>
+                    <div>
+                        <span>Tutoring - </span>
+                        <a href="">tutoring@refinityedu.org</a>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Navbar } from '../components/navbar'
 import { Hero } from '../components/contactPage/Hero'
 import { ContactForm } from '../components/contactPage/Form'
-import { Footer } from '../components/Footer'
+import { Footer } from '../components/contactPage/Footer'
 
 export default function Home() {
   return (
